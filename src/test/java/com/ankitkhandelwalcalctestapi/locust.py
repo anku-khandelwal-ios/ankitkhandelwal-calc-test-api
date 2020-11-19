259b3cea388d5715a19e0244deb2fbd6ece70636
Ankit Khandelwal <ankitkh@zeta.tech>
2020-11-19 19:26:26 +0530

    updating locust script

--- src/test/java/com/ankitkhandelwalcalctestapi/locust.py
+++ src/test/java/com/ankitkhandelwalcalctestapi/locust.py
@@ -1,15 +1,21 @@
 import os
 
 
-from locust import HttpLocust, TaskSet, task, HttpUser
+ffrom locust import HttpLocust, TaskSet, task, HttpUser
 
 
 class UserBehaviour(HttpUser):
     @task(1)
-    def launch_url(self):
-        response = self.client.get("/calc-test-api/1.0/users/1/calculations/")
+    def get_result(self):
+        response = self.client.get("/calc-test-api/1.0/users/1/calculations")
         print("Response status code:", response.status_code)
         print("Response text:", response.text)
+
+
+    @task(2)
+    def computation(self):
+        response = self.client.post("/calc-test-api/1.0/users/1/calculations",{"operand1":"1", "operator": "DIVIDE", "operand2": "0"})
+        print("output is:", response.text)
 
 class User(HttpUser):
     task_set=UserBehaviour
